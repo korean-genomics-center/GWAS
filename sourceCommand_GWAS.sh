3c9a71fb1a24ceae5101b8b352b11d543d95593b
youngjune29bhak <44763084+youngjune29bhak@users.noreply.github.com>
2020-05-31 16:14:34 +0900

    Create sourceCommand_GWAS.sh

---
+++ sourceCommand_GWAS.sh
@@ -0,0 +1,21 @@
+##
+date
+##
+
+## Main ##
+<Program: plink> \
+--bfile <Filename: plink binary file name> \
+--pheno <File: phenotype file> \
+--pheno-name <Parameter: Name of thaget phenotype> \
+--covar <File: covariate file> \
+--covar-name <Parameter: Names of covariate> \
+--linear hide-covar sex \
+--ci 0.95 --prune --memory 10000 --adjust  \
+--out <Filename: Output file name>
+##
+
+##
+date
+##
+
+
